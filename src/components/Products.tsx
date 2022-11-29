251f53df0b954c0e2d44d146b13f57f2cf13d526
dimonstr223 <dimstukmikolu@gmail.com>
2022-11-29 09:08:06 +0300

    Provider added, App wrapped, products request added

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,6 +1,17 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import useAppSelecror from '../hooks/useAppSelector'
+import { getProducts } from '../redux/products/actions/productsAction'
 
 const Products: React.FC = () => {
+	const { products } = useAppSelecror(state => state.products)
+	const dispatch: any = useDispatch()
+
+	React.useEffect(() => {
+		dispatch(getProducts())
+	}, [])
+
+	console.log(products)
 	return <div>Products</div>
 }
 
