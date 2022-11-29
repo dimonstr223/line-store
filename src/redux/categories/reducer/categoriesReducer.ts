2ad05d45dc4adacd1d1bde09f22efb1547664661
dimonstr223 <dimstukmikolu@gmail.com>
2022-11-29 13:36:37 +0300

    Categories reducer typed

--- src/redux/categories/reducer/categoriesReducer.ts
+++ src/redux/categories/reducer/categoriesReducer.ts
@@ -1,10 +1,14 @@
-const initialState: ProductsState = {
+import { CategoriesState, CategorisAction } from './../types/categoriesTypes'
+const initialState: CategoriesState = {
 	categories: [],
 	isLoading: false,
 	error: null,
 }
 
-const categoriesReducer = (state = initialState, action) => {
+const categoriesReducer = (
+	state = initialState,
+	action: CategorisAction
+): CategoriesState => {
 	switch (action.type) {
 		default:
 			return state
---
+++ src/redux/categories/types/categoriesTypes.ts
@@ -0,0 +1,27 @@
+export interface CategoriesState {
+	categories: string[]
+	isLoading: false
+	error: null | string
+}
+
+export enum CategoriesActionType {
+	SET_CATEGORIES = 'SET_CATEGORIES',
+	SET_IS_LOADING = 'SET_IS_LOADING',
+	SET_ERROR = 'SET_ERROR',
+}
+
+interface setCategory {
+	type: CategoriesActionType.SET_CATEGORIES
+	payload: string[]
+}
+interface setIsLoading {
+	type: CategoriesActionType.SET_IS_LOADING
+	payload: boolean
+}
+
+interface setError {
+	type: CategoriesActionType.SET_ERROR
+	payload: string
+}
+
+export type CategorisAction = setCategory | setError | setIsLoading
