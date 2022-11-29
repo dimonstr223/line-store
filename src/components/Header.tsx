c705f423d0b96b2a39f733e0f26341e25527a5c4
dimonstr223 <dimstukmikolu@gmail.com>
2022-11-29 11:20:47 +0300

    Header styled

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,18 +8,22 @@ import style from '../scss/components/Header.module.scss'
 const Header: React.FC = () => {
 	return (
 		<header className={style.header}>
-			<div className={style.header__logo}>
-				<img
-					className={style.logo__img}
-					src={headerLogo}
-					alt='Logo'
-					width={45}
-				/>
-				<h3 className={style.logo__text}>LineStore</h3>
-			</div>
-			<div className={style.header__cart}>
-				<img src={cartIcon} alt='Cart' width={30} />
-			</div>
+			<a href='/'>
+				<div className={style.header__logo}>
+					<img
+						className={style.logo__img}
+						src={headerLogo}
+						alt='Logo'
+						width={45}
+					/>
+					<h3 className={style.logo__text}>LineStore</h3>
+				</div>
+			</a>
+			<a href='/cart'>
+				<div className={style.header__cart}>
+					<img src={cartIcon} alt='Cart' width={30} />
+				</div>
+			</a>
 		</header>
 	)
 }
