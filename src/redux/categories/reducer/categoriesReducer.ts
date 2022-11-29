91526f2aca6878767c833683c406b492c256f89f
dimonstr223 <dimstukmikolu@gmail.com>
2022-11-29 13:30:23 +0300

    Cotegories redux directory created, reducer created

---
+++ src/redux/categories/reducer/categoriesReducer.ts
@@ -0,0 +1,14 @@
+const initialState: ProductsState = {
+	categories: [],
+	isLoading: false,
+	error: null,
+}
+
+const categoriesReducer = (state = initialState, action) => {
+	switch (action.type) {
+		default:
+			return state
+	}
+}
+
+export default categoriesReducer
