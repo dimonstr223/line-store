993203c8b5accb03b986cc0e146abd310c8399ab
dimonstr223 <dimstukmikolu@gmail.com>
2022-11-30 14:49:03 +0300

    isLoading and error types added

--- src/redux/filter/types/filterTypes.ts
+++ src/redux/filter/types/filterTypes.ts
@@ -1,14 +1,25 @@
 export interface FilterState {
 	searchValue: string
+	isLoading: boolean
 }
 
 export enum FilterActionType {
 	SET_SEARCH_VALUE = 'SET_SEARCH_VALUE',
+	SET_IS_LOADING = 'SET_IS_LOADING',
+	SET_ERROR = 'SET_ERROR',
 }
 
 interface setSearchValue {
 	type: FilterActionType.SET_SEARCH_VALUE
 	payload: string
 }
+interface setIsLoadin {
+	type: FilterActionType.SET_IS_LOADING
+	payload: boolean
+}
+interface setError {
+	type: FilterActionType.SET_ERROR
+	payload: string
+}
 
-export type FilterAction = setSearchValue
+export type FilterAction = setSearchValue | setIsLoadin | setError
