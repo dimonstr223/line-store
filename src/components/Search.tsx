ea7566e9d306ccdd5523dbbba1262ebab28d0355
dimonstr223 <dimstukmikolu@gmail.com>
2022-11-30 15:10:21 +0300

    search input styled

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -10,9 +10,6 @@ import {
 	getProudctsFromSearch,
 	setSearchValue,
 } from '../redux/filter/actions/filterActions'
-import { Dispatch } from 'redux'
-import { FilterAction } from '../redux/filter/types/filterTypes'
-import { ProductsAction } from '../redux/products/types/productsTypes'
 
 const Search: React.FC = () => {
 	const { searchValue } = useAppSelecror(state => state.filter)
@@ -39,14 +36,19 @@ const Search: React.FC = () => {
 				className={style.search__input}
 				placeholder='Search...'
 			/>
-			<div className={style.wrapper}>
-				<img
-					className={style.close__img}
-					src={closeIcon}
-					alt='Close'
-					width={20}
-				/>
-			</div>
+			{searchValue && (
+				<div
+					onClick={() => dispatch(setSearchValue(''))}
+					className={style.wrapper}
+				>
+					<img
+						className={style.close__img}
+						src={closeIcon}
+						alt='Close'
+						width={20}
+					/>
+				</div>
+			)}
 		</div>
 	)
 }
