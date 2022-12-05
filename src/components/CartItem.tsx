820793a63d29f28601e88433ff1f2502307d45b4
dimonstr223 <dimstukmikolu@gmail.com>
2022-12-05 08:19:23 +0300

    Remove from cart button with removing logic added to cartItem

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import { useAppDispatch } from '../hooks/useAppDispatch'
+import { removeFromCart } from '../redux/cart/actions/cartActions'
 import { ICartItem } from '../redux/cart/types/cartTypes'
 
 import style from '../scss/components/CartItem.module.scss'
 import calcDiscountPrice from '../utils/calcDiscountPrice'
 
 const CartItem: React.FC<ICartItem> = ({
+	id,
 	title,
 	thumbnail,
 	brand,
@@ -12,6 +15,11 @@ const CartItem: React.FC<ICartItem> = ({
 	discountPercentage,
 	quantity,
 }) => {
+	const dispatch = useAppDispatch()
+
+	const onRemoveFromCart = (id: number) => {
+		dispatch(removeFromCart(id))
+	}
 	return (
 		<div className={style.cart__item}>
 			<div className={style.img__wrapper}>
@@ -28,6 +36,13 @@ const CartItem: React.FC<ICartItem> = ({
 						{calcDiscountPrice(price, discountPercentage)}$
 					</div>
 					<div className={style.prevPrice}>{price} $</div>
+
+					<button
+						onClick={() => onRemoveFromCart(id)}
+						className={style.removeBtn}
+					>
+						Remove
+					</button>
 				</div>
 			</div>
 		</div>
