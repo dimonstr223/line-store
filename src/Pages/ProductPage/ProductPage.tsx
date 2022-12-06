b0df9abfd582e91b9fdd1e4479b85c25bfeec861
dimonstr223 <dimstukmikolu@gmail.com>
2022-12-06 09:31:44 +0300

    ProductPage component and scss created

---
+++ src/Pages/ProductPage/ProductPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import style from '../../scss/components/ProductPage.module.scss'
+
+const ProductPage: React.FC = () => {
+	return <div className={style.productPage}></div>
+}
+
+export default ProductPage
