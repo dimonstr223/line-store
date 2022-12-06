c3c9d8faa2de4c9dc51b4c34306fdef8ec27ce21
dimonstr223 <dimstukmikolu@gmail.com>
2022-12-06 10:02:56 +0300

    Navigate to products

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 
 import Header from './components/Header'
+import useAppSelecror from './hooks/useAppSelector'
 import Cart from './Pages/Cart/Cart'
 import ProductPage from './Pages/ProductPage/ProductPage'
 import Products from './Pages/Products'
@@ -9,8 +10,11 @@ import Products from './Pages/Products'
 import './scss/App.scss'
 
 const App: React.FC = () => {
+	const { products } = useAppSelecror(state => state.products)
+
 	return (
 		<div className='app'>
+			{products.length > 0 && <Navigate to='/products' />}
 			<div className='header__wrapper'>
 				<div className='container'>
 					<Header />
@@ -19,8 +23,8 @@ const App: React.FC = () => {
 			<div className='products__wrapper'>
 				<div className='container'>
 					<Routes>
-						{/* <Route path='/' element={<Products />} /> */}
-						<Route path='/' element={<ProductPage />} />
+						<Route path='/products' element={<Products />} />
+						<Route path='/products/:id' element={<ProductPage />} />
 						<Route path='/cart' element={<Cart />} />
 					</Routes>
 				</div>
