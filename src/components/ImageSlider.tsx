9fd4b4f2cb452e0416698a2a3019f0aea917bdd0
dimonstr223 <dimstukmikolu@gmail.com>
2022-12-07 09:21:23 +0300

    currentIndex state added to slider component

--- src/components/ImageSlider.tsx
+++ src/components/ImageSlider.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
+import useAppSelecror from '../hooks/useAppSelector'
 
 interface ImageSliderProps {
 	images: string[] | undefined
 }
 const ImageSlider: React.FC<ImageSliderProps> = ({ images }) => {
+	const { currentIndex } = useAppSelecror(state => state.imageSlider)
+
 	return (
 		<div>
-			<div style={{ backgroundImage: `url(${images && images[0]})` }}></div>
+			<div
+				style={{ backgroundImage: `url(${images && images[currentIndex]})` }}
+			></div>
 		</div>
 	)
 }
--- src/redux/rootReducer.ts
+++ src/redux/rootReducer.ts
@@ -2,6 +2,7 @@ import { combineReducers } from 'redux'
 import cartReducer from './cart/reducer/cartReducer'
 import categoriesReducer from './categories/reducer/categoriesReducer'
 import filterReducer from './filter/reducer/filterReducer'
+import imageSliderReducer from './imageSlider/reducer/imageSliderReducer'
 import productsReducer from './products/reducer/productsReducer'
 
 const rootReducer = combineReducers({
@@ -9,6 +10,7 @@ const rootReducer = combineReducers({
 	categories: categoriesReducer,
 	filter: filterReducer,
 	cart: cartReducer,
+	imageSlider: imageSliderReducer,
 })
 
 export type RootState = ReturnType<typeof rootReducer>
