65bbd271f853f21f83023097766c81d962b04802
dimonstr223 <dimstukmikolu@gmail.com>
2022-12-07 07:01:10 +0300

    Layout component created, Outlet component added, routes refactored

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import React from 'react'
-import { Routes, Route, Navigate } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
+import Layout from './components/Layout'
 
-import Header from './components/Header'
-import useAppSelecror from './hooks/useAppSelector'
 import Cart from './Pages/Cart/Cart'
 import ProductPage from './Pages/ProductPage/ProductPage'
 import Products from './Pages/Products'
@@ -10,25 +9,15 @@ import Products from './Pages/Products'
 import './scss/App.scss'
 
 const App: React.FC = () => {
-	const { products } = useAppSelecror(state => state.products)
-
 	return (
 		<div className='app'>
-			{products.length > 0 && <Navigate to='/products' />}
-			<div className='header__wrapper'>
-				<div className='container'>
-					<Header />
-				</div>
-			</div>
-			<div className='products__wrapper'>
-				<div className='container'>
-					<Routes>
-						<Route path='/products' element={<Products />} />
-						<Route path='/products/:id' element={<ProductPage />} />
-						<Route path='/cart' element={<Cart />} />
-					</Routes>
-				</div>
-			</div>
+			<Routes>
+				<Route path='/' element={<Layout />}>
+					<Route path='products' element={<Products />} />
+					<Route path='products/:id' element={<ProductPage />} />
+					<Route path='cart' element={<Cart />} />
+				</Route>
+			</Routes>
 		</div>
 	)
 }
---
+++ src/components/Layout.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import Header from './Header'
+
+const Layout: React.FC = () => {
+	return (
+		<>
+			<div className='header__wrapper'>
+				<div className='container'>
+					<Header />
+				</div>
+			</div>
+			<div className='content__wrapper'>
+				<div className='container'>
+					<Outlet />
+				</div>
+			</div>
+		</>
+	)
+}
+
+export default Layout
